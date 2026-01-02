99dd1035bb890d007daf7dfac4d5603227d47a24
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-02 16:06:07 +0000

    Initial Commit

---
+++ WomenInComputing/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WomenInComputing
+//
+//  Created by app-kaihatsusha on 02/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WomenInComputing/WomenInComputingApp.swift
@@ -0,0 +1,18 @@
+//
+//  WomenInComputingApp.swift
+//  WomenInComputing
+//
+//  Created by app-kaihatsusha on 02/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WomenInComputingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
